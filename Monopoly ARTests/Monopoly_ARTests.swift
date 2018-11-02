4603de091a92c65e0e661d284f3e7afe2528296d
Shuangquan Li <lishuangquan00@gmail.com>
2018-11-01 22:46:32 -0500

    Initial Commit

---
+++ Monopoly ARTests/Monopoly_ARTests.swift
@@ -0,0 +1,34 @@
+//
+//  Monopoly_ARTests.swift
+//  Monopoly ARTests
+//
+//  Created by 李双全 on 11/1/18.
+//  Copyright © 2018 Shuangquan Li. All rights reserved.
+//
+
+import XCTest
+@testable import Monopoly_AR
+
+class Monopoly_ARTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
