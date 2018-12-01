11435917a0ac1f0c06d9ec0f1dfd0f611ee88e51
Shuangquan Li <lishuangquan00@gmail.com>
2018-11-30 18:56:18 -0600

    All 32 Total Test Cases added and passed

--- Monopoly ARTests/Monopoly_ARTests.swift
+++ Monopoly ARTests/Monopoly_ARTests.swift
@@ -11,24 +11,151 @@ import XCTest
 
 class Monopoly_ARTests: XCTestCase {
 
+    var gameTest = ViewController()
+
+    let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    
+    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        gameTest = ViewController()
+        
+        
+    }
+    
+    
+    func testLocation1(){
+        XCTAssertEqual(gameTest.firstPosGenerator(num: numbers[0], who: 0), CGPoint(x: -160, y: -50))
     }
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testLocation2(){
+        XCTAssertEqual(gameTest.firstPosGenerator(num: numbers[1], who: 0), CGPoint(x: -160, y: 40))
+    }
+    
+    func testLocation3(){
+        XCTAssertEqual(gameTest.firstPosGenerator(num: numbers[2], who: 0), CGPoint(x: -160, y: 130))
+    }
+    
+    func testLocation4(){
+        XCTAssertEqual(gameTest.secondPosGenerator(num: numbers[3], who: 0), CGPoint(x: -70, y: 130))
+    }
+    
+    func testLocation5(){
+        XCTAssertEqual(gameTest.secondPosGenerator(num: numbers[4], who: 0), CGPoint(x: 20, y: 130))
+    }
+    
+    func testLocation6(){
+        XCTAssertEqual(gameTest.secondPosGenerator(num: numbers[5], who: 0), CGPoint(x: 110, y: 130))
+    }
+    
+    func testLocation7(){
+        XCTAssertEqual(gameTest.thirdPosGenerator(num: numbers[6], who: 0), CGPoint(x: 110, y: 40))
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testLocation8(){
+        XCTAssertEqual(gameTest.thirdPosGenerator(num: numbers[7], who: 0), CGPoint(x: 110, y: -50))
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testLocation9(){
+        XCTAssertEqual(gameTest.thirdPosGenerator(num: numbers[8], who: 0), CGPoint(x: 110, y: -140))
     }
+    
+    func testLocation10(){
+        XCTAssertEqual(gameTest.fourthPosGenerator(num: numbers[9], who: 0), CGPoint(x: 20, y: -140))
+    }
+    
+    func testLocation11(){
+        XCTAssertEqual(gameTest.fourthPosGenerator(num: numbers[10], who: 0), CGPoint(x: -70, y: -140))
+    }
+    
+    func testLocation12(){
+        XCTAssertEqual(gameTest.fourthPosGenerator(num: numbers[11], who: 0), CGPoint(x: -160, y: -140))
+    }
+    
+    
+    func testLocation1P2(){
+        XCTAssertEqual(gameTest.firstPosGenerator(num: numbers[0], who: 1), CGPoint(x: -130, y: -50))
+    }
+    
+    func testLocation2P2(){
+        XCTAssertEqual(gameTest.firstPosGenerator(num: numbers[1], who: 1), CGPoint(x: -130, y: 40))
+    }
+    
+    func testLocation3P2(){
+        XCTAssertEqual(gameTest.firstPosGenerator(num: numbers[2], who: 1), CGPoint(x: -130, y: 130))
+    }
+    
+    func testLocation4P2(){
+        XCTAssertEqual(gameTest.secondPosGenerator(num: numbers[3], who: 1), CGPoint(x: -40, y: 130))
+    }
+    
+    func testLocation5P2(){
+        XCTAssertEqual(gameTest.secondPosGenerator(num: numbers[4], who: 1), CGPoint(x: 50, y: 130))
+    }
+    
+    func testLocation6P2(){
+        XCTAssertEqual(gameTest.secondPosGenerator(num: numbers[5], who: 1), CGPoint(x: 140, y: 130))
+    }
+    
+    func testLocation7P2(){
+        XCTAssertEqual(gameTest.thirdPosGenerator(num: numbers[6], who: 1), CGPoint(x: 140, y: 40))
+    }
+    
+    func testLocation8P2(){
+        XCTAssertEqual(gameTest.thirdPosGenerator(num: numbers[7], who: 1), CGPoint(x: 140, y: -50))
+    }
+    
+    func testLocation9P2(){
+        XCTAssertEqual(gameTest.thirdPosGenerator(num: numbers[8], who: 1), CGPoint(x: 140, y: -140))
+    }
+    
+    func testLocation10P2(){
+        XCTAssertEqual(gameTest.fourthPosGenerator(num: numbers[9], who: 1),  CGPoint(x: 50, y: -140))
+    }
+    
+    func testLocation11P2(){
+        XCTAssertEqual(gameTest.fourthPosGenerator(num: numbers[10], who: 1), CGPoint(x: -40, y: -140))
+    }
+    
+    func testLocation12P2(){
+        XCTAssertEqual(gameTest.fourthPosGenerator(num: numbers[11], who: 1), CGPoint(x: -130, y: -140))
+    }
+    
+  
+    func testP1Score(){
+        XCTAssertEqual(gameTest.p1Score, 0)
+    }
+    
+    func testP2Score(){
+        XCTAssertEqual(gameTest.p2Score, 0)
+    }
+    
+    func testInitP1Name(){
+        XCTAssertEqual(gameTest.player1Name, "")
+        
+    }
+    
+    func testInitP2Name(){
+        XCTAssertEqual(gameTest.player2Name, "")
+        
+    }
+    
+    func testInitP1Token(){
+        XCTAssertEqual(gameTest.player1Token, "")
+        
+    }
+    
+    func testInitP2Token(){
+        XCTAssertEqual(gameTest.player2Token, "")
+    }
+    
+    func testP1House(){
+        XCTAssertEqual(gameTest.p1House0.text, "🏡")
+    }
+    
+    func testP2House(){
+        XCTAssertEqual(gameTest.p2House0.text, "🏠")
+    }
+    
 
 }
